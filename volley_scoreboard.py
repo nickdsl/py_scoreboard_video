70342f61fa55342fb403a2e3b15ff4ad579231fb
smith <nickdslk@gmail.com>
2021-03-13 17:44:18 +0600

    Added VALID_ANSWERS and VALID_INPUT support. Switched to English

--- volley_scoreboard.py
+++ volley_scoreboard.py
@@ -2,6 +2,8 @@ import pdb
 #import unicode
 #import sys
 #import os
+#корректные ответы
+VALID_ANSWERS = ['A', 'B', 'Y', 'N', 'D', '?']
 #рабочий каталог
 WORKING_DIR=".\\"
 #рабочие файлы
@@ -18,9 +20,9 @@ F_TEAM_A_SET_POINTS='TeamA_set_points.txt'
 #количество набранных очков в текущей партии второй команды
 F_TEAM_B_SET_POINTS='TeamB_set_points.txt'
 #знак подачи на экране
-F_TEAM_A_PITCH='TeamA_pitch.txt'
+F_TEAM_A_SERVE='TeamA_serve.txt'
 #знак подачи на экране
-F_TEAM_B_PITCH='TeamB_pitch.txt'
+F_TEAM_B_SERVE='TeamB_serve.txt'
 #итоги матча
 F_MATCH_RESULTS='MatchResults.txt'
 
@@ -44,15 +46,15 @@ MATCH_DATA['A']['points']=0
 #очки команды B
 MATCH_DATA['B']['points']=0
 #знак подачи команды A
-MATCH_DATA['A']['pitch']=''
+MATCH_DATA['A']['serve']=''
 #знак подачи команды B
-MATCH_DATA['B']['pitch']=''
+MATCH_DATA['B']['serve']=''
 #значок подачи который отображается на табло как знак чья подача была/будет при розыгрыше
-PITCH_SIGN='●'
+SERVE_SIGN='●'
 #флаг подает ли это команда А (в течение сета)
-A_PITCH = True
+A_SERVE = True
 #флаг - начинала ли подавать команда А в начале сета
-A_FIRST_PITCH = True
+A_FIRST_SERVE = True
 #максимальное количество очков в партии (по умолчанию)
 MAX_SET_POINTS=25
 LAST_SET_MAX_POINTS=15
@@ -81,7 +83,7 @@ def flush_results():
   fp.close()
 def write_results():
   fp = open(WORKING_DIR + "\\" + F_MATCH_RESULTS, 'ab')
-  fp.write("{}:{}\n".format(MATCH_DATA['A']['points'],MATCH_DATA['B']['points']).encode('utf-8'))
+  fp.write("{}:{}\n".format(MATCH_DATA['A']['points'], MATCH_DATA['B']['points']).encode('utf-8'))
   fp.close()
 def write_names():
   fp = open(WORKING_DIR + "\\" + F_TEAM_A_NAME, 'wb')
@@ -114,40 +116,45 @@ def write_points():
     value = str(value).encode('utf-8')
   fp.write(value)
   fp.close()
-def write_pitch():
-  fp = open(WORKING_DIR + "\\" + F_TEAM_A_PITCH, 'wb')
-  fp.write(str(MATCH_DATA['A']['pitch']).encode('utf-8'))
+def write_serve():
+  fp = open(WORKING_DIR + "\\" + F_TEAM_A_SERVE, 'wb')
+  fp.write(str(MATCH_DATA['A']['serve']).encode('utf-8'))
   fp.close()
-  fp = open(WORKING_DIR + "\\" + F_TEAM_B_PITCH, 'wb')
-  fp.write(str(MATCH_DATA['B']['pitch']).encode('utf-8'))
+  fp = open(WORKING_DIR + "\\" + F_TEAM_B_SERVE, 'wb')
+  fp.write(str(MATCH_DATA['B']['serve']).encode('utf-8'))
   fp.close()
 
 def intro():
   global MATCH_DATA
-  global A_PITCH
-  global A_FIRST_PITCH
+  global A_SERVE
+  global A_FIRST_SERVE
   global MAX_SET
   global MATCH_WIN_CONDITION
-  value = input("Введите имя первой команды:")
+  value = input("Enter team A name:")
   MATCH_DATA['A']['name'] = value
-  value = input("Введите имя второй команды:")
+  value = input("Enter team B name:")
   MATCH_DATA['B']['name'] = value
-  value = input("Подача у первой команды? (Yy/Nn)")
+  value = input("Type Y/y if team {} will serve first, else type N/n if team {} will serve:".format(
+    MATCH_DATA['A']['name'],
+    MATCH_DATA['B']['name']))
   if (value == 'Y') or (value == 'y'):
-    MATCH_DATA['A']['pitch'] = PITCH_SIGN
-    MATCH_DATA['B']['pitch'] = ''
+    MATCH_DATA['A']['serve'] = SERVE_SIGN
+    MATCH_DATA['B']['serve'] = ''
+    print("Team {} will serve the ball".format(MATCH_DATA['A']['name']))
   else:
-    MATCH_DATA['B']['pitch'] = PITCH_SIGN
-    MATCH_DATA['A']['pitch'] = ''
-    A_FIRST_PITCH = False
-    A_PITCH = A_FIRST_PITCH
-  value = input("Стандартная игра (до 3х побед из 5 партий)? (Yy/Nn)")
+    MATCH_DATA['B']['serve'] = SERVE_SIGN
+    MATCH_DATA['A']['serve'] = ''
+    A_FIRST_SERVE = False
+    A_SERVE = A_FIRST_SERVE
+    print("Team {} will serve the ball".format(MATCH_DATA['B']['name']))
+  value = input("Type Y/y for long game (5 sets) or N/n for short game (3 sets):")
   if (value == 'Y') or (value == 'y'):
     MAX_SET = 5
     MATCH_WIN_CONDITION = 3
   else:
     MAX_SET = 3
     MATCH_WIN_CONDITION = 2
+  print("Match with {} sets chosen".format(MAX_SET))
   #pdb.set_trace()
   #quit()
 #ввод начальных значений
@@ -156,7 +163,7 @@ intro()
 write_names()
 write_sets()
 write_points()
-write_pitch()
+write_serve()
 flush_results()
 #основной цикл игры. играем пока не закончится игра =)
 while not END_GAME:
@@ -169,41 +176,66 @@ while not END_GAME:
       #если да, то переопределяем количество мячей по-умолчанию для победы
       MAX_SET_POINTS = LAST_SET_MAX_POINTS
       #и тут должна быть вторая процедура розыгрыша подачи по правилам волейбола
-      value = input("Какая команда выиграла право подачи? {} - нажмите А или а".format(MATCH_DATA['A']['name']))
-      if (value == "A") or (value == "a"):
+      value = input("If team {} won the toss press A/a, else press B/b (or any key) for team {}:".format(
+        MATCH_DATA['A']['name'],
+        MATCH_DATA['B']['name']))
+      if (value.upper() == "A") or (value.upper() == "a"):
         #нужно назначить право первой подачи на команду А
         #далее по алгоритму идет процедура инверсии первой подачи.
         #следовательно если мы хотим чтобы подавала команда А, то мы должны установить флаг False 
         #т.к. он инвертируется и получится True
         #возможно это костыли, но уже лень писать строки кода
-        A_FIRST_PITCH = False
-    A_FIRST_PITCH = not A_FIRST_PITCH
-    if A_FIRST_PITCH:
-      MATCH_DATA['A']['pitch'] = PITCH_SIGN
-      MATCH_DATA['B']['pitch'] = ''
+        A_FIRST_SERVE = False
+    A_FIRST_SERVE = not A_FIRST_SERVE
+    if A_FIRST_SERVE:
+      MATCH_DATA['A']['serve'] = SERVE_SIGN
+      MATCH_DATA['B']['serve'] = ''
     else:
-      MATCH_DATA['B']['pitch'] = PITCH_SIGN
-      MATCH_DATA['A']['pitch'] = ''
-    A_PITCH = A_FIRST_PITCH
-    write_pitch()
+      MATCH_DATA['B']['serve'] = SERVE_SIGN
+      MATCH_DATA['A']['serve'] = ''
+    A_SERVE = A_FIRST_SERVE
+    write_serve()
   #пока не закончится сет выполнять цикл
   while not END_SET:
-    value = input("Кто выиграл розыгрыш? Если {} то нажать A или a, иначе выиграла команда {} и нужно нажать B или b".format(MATCH_DATA['A']['name'],MATCH_DATA['B']['name']))
+    #базовая обработка ввода
+    VALID_INPUT = False
+    while not VALID_INPUT:
+      value = input("Who scored? If {} then press A/a, else B/b for {}. Type ? for more options".format(
+        MATCH_DATA['A']['name'],
+        MATCH_DATA['B']['name']))
+      if value.upper() in VALID_ANSWERS:
+        VALID_INPUT = True
+      #case - A - point to team A
+      #case - B - point to team B
+      #case - D - double fault
+      #case - ? - all options / help
+    if (value.upper() == '?'):
+      print('Valid options:')
+      print("A - team {} scored".format(MATCH_DATA['A']['name']))
+      print("B - team {} scored".format(MATCH_DATA['B']['name']))
+      print("D - double fault. No score. Team have to serve again.")
+      print("Y - yes")
+      print("N - no")
+      print("? - show valid options")
+      continue
+    if (value.upper() == 'D'):
+      print("Double fault")
+      continue
     # проверка введенного значения и набор очка одной из команд
-    if (value == 'A') or (value == 'a'):
+    if (value.upper() == 'A'):
       MATCH_DATA['A']['points'] = MATCH_DATA['A']['points'] + 1
-      if not A_PITCH:
-        A_PITCH = True
-        MATCH_DATA['A']['pitch'] = PITCH_SIGN
-        MATCH_DATA['B']['pitch'] = ''
-        write_pitch()
-    else:
+      if not A_SERVE:
+        A_SERVE = True
+        MATCH_DATA['A']['serve'] = SERVE_SIGN
+        MATCH_DATA['B']['serve'] = ''
+        write_serve()
+    if (value.upper() == 'B'):
       MATCH_DATA['B']['points'] = MATCH_DATA['B']['points'] + 1
-      if A_PITCH:
-        A_PITCH = False
-        MATCH_DATA['B']['pitch'] = PITCH_SIGN
-        MATCH_DATA['A']['pitch'] = ''
-        write_pitch()
+      if A_SERVE:
+        A_SERVE = False
+        MATCH_DATA['B']['serve'] = SERVE_SIGN
+        MATCH_DATA['A']['serve'] = ''
+        write_serve()
     #зафиксируем изменения на экране
     write_points()
     # проверка условия победы в сете/матче
@@ -221,7 +253,7 @@ while not END_GAME:
       #запишем изменения в сетах
       write_sets()
       write_results()
-      if (CURRENT_SET == MAX_SET) or ((MATCH_DATA['A']['sets'] == MATCH_WIN_CONDITION) or (MATCH_DATA['A']['sets'] == MATCH_WIN_CONDITION)):
+      if (CURRENT_SET == MAX_SET) or ((MATCH_DATA['A']['sets'] == MATCH_WIN_CONDITION) or (MATCH_DATA['B']['sets'] == MATCH_WIN_CONDITION)):
         #игра окончена. нужно выходить из всех циклов
         END_GAME = True
       else:
@@ -229,9 +261,9 @@ while not END_GAME:
         #увеличить счетчик сетов
         #обнулить значения очков в сете и начать новый сет
         write_points()
-        value = input("Партия закончена. Чтобы перейти к следующей нажмите ВВОД")
+        value = input("Set is over. Press ENTER to continue")
         CURRENT_SET = CURRENT_SET + 1
         MATCH_DATA['A']['points'] = 0
         MATCH_DATA['B']['points'] = 0
         write_points()
-value = input("Игра окончена. Для выхода нажмите ВВОД")
+value = input("Match is over. Press ENTER to exit")
